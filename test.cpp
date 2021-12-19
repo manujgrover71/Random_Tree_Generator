7e10ee6d58130356de1db37ee36db5bfae805d75
Manuj Grover <manujgrover71@gmail.com>
2021-12-19 11:09:51 +0530

    Implemented O(H) time and O(1) space random tree

--- test.cpp
+++ test.cpp
@@ -15,27 +15,25 @@ public:
 int main() {
 
     // default constructor.
-    Tree<RandNumbers> t(3, 3);
+    Tree<RandNumbers> t(2, 4);
     
     // adding class to tree.
     for(int i = 0; i < 50; i++) {
-        t.addNode(RandNumbers(rand() % 100, rand() % 100));
+        cout << t.addNode(RandNumbers(rand() % 100, rand() % 100)) << '\n';
     }
 
     // taking the edges.
     auto edges = t.getEdges();
     
-    set<long long> st;
-    
     for(auto edge : edges) {
         auto *parent = edge.first;
         auto *child = edge.second;
         
-        long long parentId = parent->getId();
-        long long childId = child->getId();
+        long long parentId = parent->id;
+        long long childId = child->id;
         
-        int parentDepth = parent->getDepth();
-        int childDepth = child->getDepth();
+        int parentDepth = parent->depth;
+        int childDepth = child->depth;
         
         cout << parentId << ' ' << childId << '\n';
     }
--- tree_generator.h
+++ tree_generator.h
@@ -5,20 +5,27 @@
 #include <random>
 #include <ctime>
 #include <queue>
+#include <stdexcept>
 #include <chrono>
 using namespace std;
 using namespace chrono;
 
 namespace tree_gen {
-    
+
     mt19937 gen(time(0));
     uniform_int_distribution<long long> distr(0, 1e18);
-    
+
+    const long long current_time = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count();
+
     int getRandInRange(long long min, long long max) {
         if(min == 0 && max == 0) {
             return 0;
         }
         return min + (distr(gen) % (max - min + 1));
+    }
+
+    long long getUUID() {
+        return current_time + getRandInRange(1e16, 1e17 - 1);
     }
 
     template<typename T>
@@ -36,65 +43,12 @@ namespace tree_gen {
         // current depth
         int depth{};
 
-        // boolean to represent if mode childen could be added.
-        bool child_addable{};
-
-    public:
         explicit Node(T data) : data(data) {
-            
-            // generate a unique id.
-            long long current_time = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count();
-            this->id = current_time + getRandInRange(1e16, 1e17 - 1);
-
+            // get a unique id.
+            this->id = getUUID();
             // initialize the requirements.
             this->out = 0;
             this->depth = -1;
-            this->child_addable = true;
-        }
-        
-        // getter for id.
-        long long getId() {
-            return this->id;
-        }
-
-        // getter for out degree
-        int getOutDegree() {
-            return this->out;
-        }
-
-        // increment out degree of current node
-        void incrementOutDegree() {
-            this->out++;
-        }
-
-        // getter for depth of current node.
-        int getDepth() {
-            return this->depth;
-        }
-
-        // setter for depth of current node.
-        void setDepth(int depth) {
-            this->depth = depth;
-        }
-
-        // getter for addableChildren bool
-        bool canAddChildren() {
-            return child_addable;
-        }
-
-        // setter for addableChildren bool
-        void setchild_addable(bool status) {
-            this->child_addable = status;
-        }
-
-        // add child.
-        void addChild(Node<T> *child) {
-            children.push_back(child);
-        }
-
-        // getter for children
-        vector<Node *> getChildren() {
-            return this->children;
         }
     };
 
@@ -102,10 +56,16 @@ namespace tree_gen {
     class Tree {
 
         // root of tree
-        Node<N> *head;      
+        Node<N> *head;
 
-        //  number of nodes which can not have more children.
-        int non_addable{};
+        // all the node paths that are avaiable.
+        vector<int> available_node_paths;
+        
+        int curr_power{};
+        int next_power{};
+        
+        // upper_bound on the paths.
+        int max_power{};
 
         // number of nodes in tree.
         int total_nodes{};
@@ -115,9 +75,6 @@ namespace tree_gen {
         int max_depth{};
         int max_out{};
 
-        // for generating random numbers
-        
-        
         static const int INF = 1e9;
 
     public:
@@ -126,43 +83,48 @@ namespace tree_gen {
             this->head = nullptr;
 
             // initialize the variables.
-            this->non_addable = 0;
             this->total_nodes = 0;
 
-            // for default constructor
-            this->max_out = INF;
-            this->max_depth = INF;
-            
+            // for default constructor, default values.
+            this->max_out = 5; 
+            this->max_depth = 10;
         }
 
 
         explicit Tree(int max_out_degree, int max_depth) : Tree() {
-        
+
             if(max_depth < 1) {
                 throw runtime_error("Max Depth should be greater than 1");
             }
-            
+
             if(max_out_degree < 0) {
                 throw runtime_error("Max out degree should be positive");
             }
-        
+
             this->max_out = max_out_degree;
             this->max_depth = max_depth;
-            
+
+            this->next_power = max_out;
+            this->curr_power = 0;
+            this->max_power = pow(this->max_out, this->max_depth - 1);
         }
 
         // function to add data as node in tree
         // returns true if node is added, else false.
         bool addNode(N data) {
-            // create Node class with given data.
+            // add more node paths.
+            addPaths();
+            
             auto *current_node = new Node<N>(data);
+
+            current_node->children.resize(max_out, nullptr);
 
             // if head is null, make current_node as head.
             if(head == nullptr) {
                 head = current_node;
 
                 // make depth 1.
-                head->setDepth(1);
+                head->depth = 1;
 
                 // increase total_nodes.
                 this->total_nodes++;
@@ -171,11 +133,44 @@ namespace tree_gen {
                 return true;
             }
 
-            // probability of adding current_node at any parent.
-            int probab = total_nodes - non_addable - head->canAddChildren();
+            if(available_node_paths.empty()) {
+                return false;
+            }
+            
+            Node<N> *parent = head;
+            
+            // get any avaiable path
+            int toChoose = getRandInRange(0, available_node_paths.size() - 1);
+            swap(available_node_paths[toChoose], available_node_paths.back());
+            toChoose = available_node_paths.back();
+            available_node_paths.pop_back();
+            
+            // convert into b base.
+            string inBase;
+            while(toChoose > 0) {
+                inBase += char((toChoose % max_out) + '0');
+                toChoose /= max_out;
+            }
+            reverse(inBase.begin(), inBase.end());
+            
+            // move head pointer to the path and add it to the appropriate place.
+            int n = inBase.size();
+            for(int i = 0; i < n; i++) {
+                if(i == n - 1) {
+                    parent->children[inBase[i] - '0'] = current_node;
+                } else {
+                    parent = parent->children[inBase[i] - '0'];
+                }
+            }
 
-            // utility function to add node.
-            return addNodeUtil(current_node, head, probab);
+            // increment out degree.
+            parent->out++;
+            
+            // set the depth of current node.
+            current_node->depth = parent->depth + 1;
+            
+            // node added.
+            return true;
         }
 
         // function to return edges in form of ids of node.
@@ -193,7 +188,9 @@ namespace tree_gen {
                     auto current_node = q.front();
                     q.pop();
 
-                    for (auto child : current_node->getChildren()) {
+                    for (auto child : current_node->children) {
+                        if(child == nullptr)
+                            continue;
                         edges.push_back({current_node, child});
 
                         q.push(child);
@@ -206,88 +203,19 @@ namespace tree_gen {
 
     private:
 
+        void addPaths() {
 
-        bool addNodeUtil(Node<N> *current_node, Node<N> *parent, int &probab) {
+            if(!available_node_paths.empty())
+                return;
 
-            // check if we could add current element as child of parent node.
-            if(parent->canAddChildren() && isAddable(parent, probab)) {
-
-                // add child to parent.
-                parent->addChild(current_node);
-
-                // increase out degree of parent node.
-                parent->incrementOutDegree();
-
-                // set depth of curret node.
-                current_node->setDepth(parent->getDepth() + 1);
-
-                // increment the total nodes count.
-                total_nodes++;
-
-                // check of addability of parent and current node.
-                if(!canAddMore(parent)) {
-                    non_addable++;
-                    parent->setchild_addable(false);
-                }
-
-                if(!canAddMore(current_node)) {
-                    non_addable++;
-                    current_node->setchild_addable(false);
-                }
-
-                // node is added.
-                return true;
+            while(curr_power < next_power && available_node_paths.size() < 10) {
+                available_node_paths.push_back(curr_power++);
             }
 
-            // traverse down the path to check for other places.
-            if(parent->getDepth() + 1 <= max_depth) {
-
-                for(auto child : parent->getChildren()) {
-                    // update the probability
-                    probab -= child->canAddChildren();
-
-                    // check children of parent node.
-                    if(addNodeUtil(current_node, child, probab)) {
-                        return true;
-                    }
-                }
+            if(next_power < max_power - 1 && curr_power == next_power) {
+                next_power *= max_out;
             }
 
-            // couldn't add in current subtree.
-            return false;
-        }
-
-        // function to check if we could add more child to current node.
-        // apply extra constraints as needed!!
-        bool canAddMore(Node<N> *current_node) {
-
-            // depth check
-            if(current_node->getDepth() == max_depth)
-                return false;
-
-            // out degree check
-            if(current_node->getOutDegree() == max_out)
-                return false;
-
-            return true;
-        }
-
-        // function to check if we could add child to current node.
-        // apply extra constraints as needed!!
-        bool isAddable(Node<N> *parent, int probab) {
-
-            // depth check
-            if(parent->getDepth() + 1 > max_depth) {
-                return false;
-            }
-
-            // max out degree check
-            if(parent->getOutDegree() + 1 > max_out) {
-                return false;
-            }
-
-            // randomness
-            return getRandInRange(0, probab) == 0;
         }
     };
 }
